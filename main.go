96da3e2a9827ba4bb73d5a68c78eceb8f10cfd81
DevJoshBrown <devjoshbrown@gmail.com>
2026-04-16 10:01:50 +0100

    added the first custom handler for 'healthz' to check server readiness

--- main.go
+++ main.go
@@ -1,5 +1,6 @@
 package main
 
+//BUILD :  go build -o out && ./out
 import (
 	"fmt"
 	"net/http"
@@ -8,14 +9,22 @@ import (
 type Server struct {
 }
 
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
 func main() {
 	fmt.Println("Running main.go")
 	mux := http.NewServeMux()
+	mux.HandleFunc("/healthz", handlerReadiness)
 
 	server := &http.Server{
 		Handler: mux,
 		Addr:    ":8080",
 	}
-
+	fileServerHandler := http.FileServer(http.Dir("."))
+	mux.Handle("/app/", http.StripPrefix("/app", fileServerHandler))
 	server.ListenAndServe()
 }
