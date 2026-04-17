7c068e2e4194cb2874b520da309b9b19c2d91c45
DevJoshBrown <devjoshbrown@gmail.com>
2026-04-17 10:22:48 +0100

    added handler for request counter and a reset handler

--- main.go
+++ main.go
@@ -4,9 +4,34 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync/atomic"
 )
 
 type Server struct {
+}
+
+// used to hold any stateful in-memory data that we want to keep track of.
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1) // this processes before the handler runs
+		next.ServeHTTP(w, r)      // run the wrapped handler
+		// you could add processes to occurr after the handler like logging time to complete or errors etc.
+	})
+}
+
+func (cfg *apiConfig) handlerRequestCounter(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write(fmt.Appendf(nil, "Hits: %d", cfg.fileserverHits.Load()))
+}
+
+func (cfg *apiConfig) handlerResetCounter(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.WriteHeader(http.StatusOK)
 }
 
 func handlerReadiness(w http.ResponseWriter, r *http.Request) {
@@ -17,14 +42,20 @@ func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	fmt.Println("Running main.go")
+	apiCfg := &apiConfig{}
 	mux := http.NewServeMux()
-	mux.HandleFunc("/healthz", handlerReadiness)
 
 	server := &http.Server{
 		Handler: mux,
 		Addr:    ":8080",
 	}
-	fileServerHandler := http.FileServer(http.Dir("."))
-	mux.Handle("/app/", http.StripPrefix("/app", fileServerHandler))
+
+	fileServerHandler := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(fileServerHandler))
+
+	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.HandleFunc("/metrics", apiCfg.handlerRequestCounter)
+	mux.HandleFunc("/reset", apiCfg.handlerResetCounter)
+
 	server.ListenAndServe()
 }
